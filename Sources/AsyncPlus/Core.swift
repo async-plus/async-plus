512b69e8e7bd86c8cae937138b56e86acc318a8c
Gabe Montague <montague.gabe@gmail.com>
2022-02-27 22:56:29 -0500

    Added methods for throwing and async contexts

--- Sources/AsyncPlus/Core.swift
+++ Sources/AsyncPlus/Core.swift
@@ -21,17 +21,60 @@ protocol NodeAsync: Node where When == Async {}
 protocol NodeNonFailableInstant: NodeNonFailable, NodeInstant {
     var result: T { get }
 }
+extension NodeNonFailableInstant {
+    public var value: T {
+        return result
+    }
+}
 
 protocol NodeFailableInstant: NodeFailable, NodeInstant {
     var result: SResult<T> { get }
+}
+extension NodeFailableInstant {
+    public func `throws`() throws -> T {
+        switch result {
+        case .success(let value):
+            return value
+        case .failure(let error):
+            throw error
+        }
+    }
+    
+    public func optional() -> T? {
+        switch result {
+        case .success(let value):
+            return value
+        case .failure(_):
+            return nil
+        }
+    }
 }
 
 protocol NodeNonFailableAsync: NodeNonFailable, NodeAsync {
     var task: NonFailableTask<T> { get }
 }
+extension NodeNonFailableAsync {
+    public func async() async -> T {
+        return await task.value
+    }
+}
 
 protocol NodeFailableAsync: NodeFailable, NodeAsync {
     var task: FailableTask<T> { get }
+}
+extension NodeFailableAsync {
+    public func asyncThrows() async throws -> T {
+        return try await task.value
+    }
+    
+    public func asyncOptional() async -> T? {
+        switch await task.result {
+        case .success(let value):
+            return value
+        case .failure(_):
+            return nil
+        }
+    }
 }
 
 // START attempt
