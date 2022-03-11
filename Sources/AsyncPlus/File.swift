1056612570853ee56b01e5892201e184fa59b1f9
Gabe Montague <montague.gabe@gmail.com>
2022-03-11 15:10:33 -0500

    Whittle...

--- Sources/AsyncPlus/File.swift
+++ Sources/AsyncPlus/File.swift
@@ -1,54 +1,27 @@
 import Foundation
 
 // TYPE FLAGS
-public class IsFailableFlag {}
-public final class NonFailableFlag: IsFailableFlag {}
-public final class FailableFlag: IsFailableFlag {}
-
-public class WhenFlag {}
-public final class InstantFlag: WhenFlag {}
-public final class AsyncFlag: WhenFlag {}
 public class StageFlag {}
-
 public class Chainable: StageFlag {}
 public final class Thenable: Chainable {}
-public class Caught: Chainable {}
-public final class PartiallyCaught: Caught {}
-public final class CompletelyCaught: Caught {}
-public final class Finalized: StageFlag {}
+public final class CompletelyCaught: Chainable {}
 
 // NODE
 public protocol Node {
-    associatedtype T
-    associatedtype Fails: IsFailableFlag
-    associatedtype When: WhenFlag
     associatedtype Stage: StageFlag
 }
 
-public typealias SimpleResult<Success> = Swift.Result<Success, Error>
+public class AnyStageResult<Stage: StageFlag>: Node {
 
-public class AnyStageResult<T, Stage: StageFlag>: Node {
-    public typealias Fails = FailableFlag
-    public typealias When = InstantFlag
-    
-    public let result: SimpleResult<T>
-    init(_ result: SimpleResult<T>) {
-        self.result = result
-    }
 }
 
-public typealias Result<T> = AnyStageResult<T, Thenable>
-public typealias PartiallyCaughtResult<T> = AnyStageResult<T, PartiallyCaught>
-public typealias CaughtResult<T> = AnyStageResult<T, CompletelyCaught>
-public typealias FinalizedResult<T> = AnyStageResult<T, Finalized>
+public typealias Result = AnyStageResult<Thenable>
+public typealias CaughtResult = AnyStageResult<CompletelyCaught>
 
 // CATCH
-public protocol Catchable: Node where Stage: Chainable, Fails == FailableFlag {
+public protocol Catchable: Node where Stage: Chainable {
     
     associatedtype SelfCaught: Node where
-    SelfCaught.T == T,
-    SelfCaught.When == When,
-    SelfCaught.Fails == Fails,
     SelfCaught.Stage == CompletelyCaught
     
     @discardableResult
@@ -57,42 +30,28 @@ public protocol Catchable: Node where Stage: Chainable, Fails == FailableFlag {
 }
 
 extension AnyStageResult: Catchable where Stage: Chainable {
-    public typealias SelfCaught = CaughtResult<T>
+    public typealias SelfCaught = CaughtResult
     
     @discardableResult
-    public func catchEscaping(_ body: @escaping (Error) -> ()) -> CaughtResult<T> {
+    public func catchEscaping(_ body: @escaping (Error) -> ()) -> CaughtResult {
         
-        if case .failure(let error) = result {
-            body(error)
-        }
-        return CaughtResult(result)
+        return CaughtResult()
     }
 }
 
 // RECOVER
-public protocol Recoverable: Node where Fails == FailableFlag, Stage == Thenable {
+public protocol Recoverable: Node where Stage == Thenable {
 
     associatedtype SelfNode: Node where
-    SelfNode.T == T,
-    SelfNode.When == When,
-    SelfNode.Fails == FailableFlag,
     SelfNode.Stage == Thenable
 
-    func recoverEscaping(_ body: @escaping (Error) throws -> T) -> SelfNode
+    func recoverEscaping(_ body: @escaping (Error) throws -> ()) -> SelfNode
 }
 
+// Comment this out for passing build
 extension AnyStageResult: Recoverable where Stage == Thenable {
 
-    public func recoverEscaping(_ body: @escaping (Error) throws -> T) -> Result<T> {
-        switch result {
-        case .success(let value):
-            return Result(.success(value))
-        case .failure(let errorOriginal):
-            do {
-                return Result(.success(try body(errorOriginal)))
-            } catch {
-                return Result(.failure(error))
-            }
-        }
+    public func recoverEscaping(_ body: @escaping (Error) throws -> ()) -> Result {
+        return Result()
     }
 }
