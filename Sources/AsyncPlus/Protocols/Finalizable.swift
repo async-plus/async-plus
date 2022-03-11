adeeaa2465b23a96079a8330a0ff1b1e4bf95cbc
Gabe Montague <montague.gabe@gmail.com>
2022-03-11 14:32:42 -0500

    Annoying: swift is not letting us conform the same class to multiple protocols depending on different `where` conditions

--- Sources/AsyncPlus/Protocols/Finalizable.swift
+++ Sources/AsyncPlus/Protocols/Finalizable.swift
@@ -1,42 +1,42 @@
-import Foundation
-
-protocol Finalizable: Node { // where Stage == CompletelyCaught OR Fails == NonFailableFlag
-    
-    associatedtype SelfFinalized: Node where
-    SelfFinalized.T == T,
-    SelfFinalized.When == When,
-    SelfFinalized.Fails == Fails,
-    SelfFinalized.Stage == Finalized
-    
-    associatedtype SelfAsyncFinalized: Node where
-    SelfAsyncFinalized.T == T,
-    SelfAsyncFinalized.When == AsyncFlag,
-    SelfAsyncFinalized.Fails == Fails,
-    SelfAsyncFinalized.Stage == Finalized
-    
-    @discardableResult
-    func finallyEscaping(_ body: @escaping () -> ()) -> SelfFinalized
-    
-    @discardableResult
-    func finally(_ body: @escaping () async -> ()) -> SelfAsyncFinalized
-}
-
-extension AnyStageValue: Finalizable {
-}
-
-extension AnyStageResult: Finalizable where Stage == CompletelyCaught {
-}
-
-extension AnyStageGuarantee: Finalizable {
-    @discardableResult
-    public func finallyEscaping(_ body: @escaping () -> ()) -> FinalizedGuarantee<T> {
-        return finally(body)
-    }
-}
-
-extension AnyStagePromise: Finalizable where Stage == CompletelyCaught {
-    @discardableResult
-    public func finallyEscaping(_ body: @escaping () -> ()) -> FinalizedPromise<T> {
-        return finally(body)
-    }
-}
+//import Foundation
+//
+//protocol Finalizable: Node { // where Stage == CompletelyCaught OR Fails == NonFailableFlag
+//    
+//    associatedtype SelfFinalized: Node where
+//    SelfFinalized.T == T,
+//    SelfFinalized.When == When,
+//    SelfFinalized.Fails == Fails,
+//    SelfFinalized.Stage == Finalized
+//    
+//    associatedtype SelfAsyncFinalized: Node where
+//    SelfAsyncFinalized.T == T,
+//    SelfAsyncFinalized.When == AsyncFlag,
+//    SelfAsyncFinalized.Fails == Fails,
+//    SelfAsyncFinalized.Stage == Finalized
+//    
+//    @discardableResult
+//    func finallyEscaping(_ body: @escaping () -> ()) -> SelfFinalized
+//    
+//    @discardableResult
+//    func finally(_ body: @escaping () async -> ()) -> SelfAsyncFinalized
+//}
+//
+//extension AnyStageValue: Finalizable {
+//}
+//
+//extension AnyStageResult: Finalizable where Stage == CompletelyCaught {
+//}
+//
+//extension AnyStageGuarantee: Finalizable {
+//    @discardableResult
+//    public func finallyEscaping(_ body: @escaping () -> ()) -> FinalizedGuarantee<T> {
+//        return finally(body)
+//    }
+//}
+//
+//extension AnyStagePromise: Finalizable where Stage == CompletelyCaught {
+//    @discardableResult
+//    public func finallyEscaping(_ body: @escaping () -> ()) -> FinalizedPromise<T> {
+//        return finally(body)
+//    }
+//}
--- Sources/AsyncPlus/Protocols/Recoverable.swift
+++ Sources/AsyncPlus/Protocols/Recoverable.swift
@@ -1,36 +1,51 @@
-//import Foundation
+import Foundation
+
+public protocol Recoverable: Node where Fails == FailableFlag, Stage == Thenable {
+
+    associatedtype SelfNonFailable: Node where
+    SelfNonFailable.T == T,
+    SelfNonFailable.When == When,
+    SelfNonFailable.Fails == NonFailableFlag,
+    SelfNonFailable.Stage == Thenable
+
+    associatedtype SelfNode: Node where
+    SelfNode.T == T,
+    SelfNode.When == When,
+    SelfNode.Fails == FailableFlag,
+    SelfNode.Stage == Thenable
+
+    associatedtype SelfNonFailableAsync: Node where
+    SelfNonFailableAsync.T == T,
+    SelfNonFailableAsync.When == AsyncFlag,
+    SelfNonFailableAsync.Fails == NonFailableFlag,
+    SelfNonFailableAsync.Stage == Thenable
+
+    associatedtype SelfAsync: Node where
+    SelfAsync.T == T,
+    SelfAsync.When == AsyncFlag,
+    SelfAsync.Fails == FailableFlag,
+    SelfAsync.Stage == Thenable
+
+    func recoverEscaping(_ body: @escaping (Error) -> T) -> SelfNonFailable
+
+    func recoverEscaping(_ body: @escaping (Error) throws -> T) -> SelfNode
+
+    func recover(_ body: @escaping (Error) async -> T) -> SelfNonFailableAsync
+
+    func recover(_ body: @escaping (Error) async throws -> T) -> SelfAsync
+}
+
+//extension AnyStageResult: Recoverable where Stage == Thenable {
+//    
+//}
 //
-//public protocol Recoverable: Node where Fails == FailableFlag, Stage == Thenable {
+//extension AnyStagePromise: Recoverable where Stage == Thenable {
 //    
-//    associatedtype SelfNonFailable: Node where
-//    SelfNonFailable.T == T,
-//    SelfNonFailable.When == When,
-//    SelfNonFailable.Fails == NonFailableFlag,
-//    SelfNonFailable.Stage == Thenable
-//    
-//    associatedtype SelfNode: Node where
-//    SelfNode.T == T,
-//    SelfNode.When == When,
-//    SelfNode.Fails == FailableFlag,
-//    SelfNode.Stage == Thenable
-//    
-//    associatedtype SelfNonFailableAsync: Node where
-//    SelfNonFailableAsync.T == T,
-//    SelfNonFailableAsync.When == AsyncFlag,
-//    SelfNonFailableAsync.Fails == NonFailableFlag,
-//    SelfNonFailableAsync.Stage == Thenable
-//    
-//    associatedtype SelfAsync: Node where
-//    SelfAsync.T == T,
-//    SelfAsync.When == AsyncFlag,
-//    SelfAsync.Fails == FailableFlag,
-//    SelfAsync.Stage == Thenable
-//    
-//    func recoverEscaping(_ body: @escaping (Error) -> T) -> SelfNonFailable
+//    public func recoverEscaping(_ body: @escaping (Error) -> T) -> Guarantee<T> {
+//        return recover(body)
+//    }
 //
-//    func recoverEscaping(_ body: @escaping (Error) throws -> T) -> SelfNode
-//
-//    func recover(_ body: @escaping (Error) async -> T) -> SelfNonFailableAsync
-//
-//    func recover(_ body: @escaping (Error) async throws -> T) -> SelfAsync
+//    public func recoverEscaping(_ body: @escaping (Error) throws -> T) -> Promise<T> {
+//        return recover(body)
+//    }
 //}
